a280ba333d5ca5838234580eef3694b653e85c4c
Ibtasham Ali <ibtasham.ali@emerssive.com>
2022-02-23 15:23:11 +0500

    set up axios and qs packages and generic api file

---
+++ src/generic-services/api.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import qs from "qs";
+// import auth from "../auth";
+
+class Api {
+  execute(url,method, dataObject={}) {
+    return new Promise(function (resolve, reject) {
+      const authAxios = axios.create({
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('access_token')}`
+        }
+      })
+      authAxios({
+        method: method,
+        url: url,
+        data: qs.stringify(dataObject),
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+        }
+      }).then((res) =>{
+        resolve(res);
+      }).catch((err) =>{
+        if (err.response.status === 401) {
+          // auth.logout();
+        }
+        reject(err);
+      })
+    })
+  }
+
+  get(url) {
+    return new Promise((resolve, reject )=> {
+      axios.get(url).then(res => {
+        resolve(res);
+      }).catch(err => {
+        reject(err);
+      });
+    })
+  }
+}
+
+export default new Api();
