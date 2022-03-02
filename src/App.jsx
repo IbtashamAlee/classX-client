6804db3ca999be8e9aa895d25fa96b12e216ae47
Ibtasham Ali <ibtasham.ali@emerssive.com>
2022-03-02 13:55:33 +0500

    setup redux store and all its functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+import {Routes, Route, Outlet, Link} from "react-router-dom";
 
 export default function App() {
   return (
@@ -18,15 +18,15 @@ export default function App() {
             parent route paths, and nested route elements render inside
             parent route elements. See the note about <Outlet> below. */}
         <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Layout/>}>
+            <Route index element={<Home/>}/>
+            <Route path="about" element={<About/>}/>
+            <Route path="dashboard" element={<Dashboard/>}/>
 
             {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
                 routes for. */}
-            <Route path="*" element={<NoMatch />} />
+            <Route path="*" element={<NoMatch/>}/>
           </Route>
         </Routes>
       </div>
@@ -55,12 +55,12 @@ function Layout() {
           </ul>
         </nav>
 
-        <hr />
+        <hr/>
 
         {/* An <Outlet> renders whatever child route is currently active,
           so you can think about this <Outlet> as a placeholder for
           the child routes we defined above. */}
-        <Outlet />
+        <Outlet/>
       </div>
   );
 }
