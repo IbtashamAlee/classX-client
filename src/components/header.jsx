6d29310985a224cb464e5efcbe5ca130c736f25f
Ibtasham Ali <ibtasham.ali@emerssive.com>
2022-04-25 01:16:48 +0500

    adds add class, join class button in header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,6 +7,8 @@ import {SearchIcon} from "@heroicons/react/solid";
 import {Menu, Transition} from "@headlessui/react";
 import Sidebar from "./sidebar";
 import {useNavigate} from "react-router-dom";
+import {IconButton} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -29,6 +31,13 @@ export function Header(props) {
     { name: 'Your Profile', href: '#', click: '' },
     { name: 'Settings', href: '#', click: '' },
     { name: 'Sign out', href: '#', click: signOut },
+  ]
+
+  const addNavigation = [
+    { name: 'Create class', href: '#', click: '' },
+    { name: 'Create class in department', href: '#', click: '' },
+    { name: 'Create institute', href: '#', click: '' },
+    { name: 'Join class', href: '#', click: '' },
   ]
 
   return (
@@ -69,25 +78,62 @@ export function Header(props) {
                   </form>
                 </div>
             }
-            <div className="ml-4 flex items-center md:ml-6">
-              <button
-                  type="button"
-                  className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
+            <div className="ml-4 flex items-center md:ml-6 space-x-3">
+              <Menu as="div" className="relative">
+                <div>
+                  <Menu.Button>
+                    <span className="sr-only">Open user menu</span>
+                    <IconButton type="button">
+                      <span className="sr-only">View notifications</span>
+                      <AddIcon className="h-6 w-6" aria-hidden="true" />
+                    </IconButton>
+                  </Menu.Button>
+                </div>
+                <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                >
+                  <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 py-1 focus:outline-none">
+                    {addNavigation.map((item) => (
+                        <Menu.Item key={item.name} onClick={item.click}>
+                          {({ active }) => (
+                              <span
+                                  className={classNames(
+                                      active ? 'bg-gray-100' : '',
+                                      'block py-2 px-4 text-sm text-gray-700 select-none cursor-pointer'
+                                  )}
+                              >
+                                {item.name}
+                              </span>
+                          )}
+                        </Menu.Item>
+                    ))}
+                  </Menu.Items>
+                </Transition>
+              </Menu>
+
+              <IconButton type="button">
                 <span className="sr-only">View notifications</span>
                 <BellIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
+              </IconButton>
 
               {/* Profile dropdown */}
               <Menu as="div" className="ml-3 relative">
                 <div>
-                  <Menu.Button className="max-w-xs flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                  <Menu.Button>
                     <span className="sr-only">Open user menu</span>
-                    <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                    />
+                    <IconButton>
+                      <img
+                          className="h-8 w-8 rounded-full"
+                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                          alt=""
+                      />
+                    </IconButton>
                   </Menu.Button>
                 </div>
                 <Transition
@@ -103,15 +149,14 @@ export function Header(props) {
                     {userNavigation.map((item) => (
                         <Menu.Item key={item.name} onClick={item.click}>
                           {({ active }) => (
-                              <a
-                                  href={item.href}
+                              <span
                                   className={classNames(
                                       active ? 'bg-gray-100' : '',
-                                      'block py-2 px-4 text-sm text-gray-700'
+                                      'block py-2 px-4 text-sm text-gray-700 select-none cursor-pointer'
                                   )}
                               >
                                 {item.name}
-                              </a>
+                              </span>
                           )}
                         </Menu.Item>
                     ))}
