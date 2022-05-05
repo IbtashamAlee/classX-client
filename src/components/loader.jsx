0379cb99a41b0264a5591412877b14bcf533e0db
Ibtasham Ali <ibtasham.ali@emerssive.com>
2022-05-05 18:18:37 +0500

    loader fix

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export function Loader() {
   return (
-      <div id="loader" className="bg-white bg-opacity-50 fixed flex justify-center items-center h-full w-full" style={{"zIndex": "1500"}}>
+      <div id="loader" className="bg-white bg-opacity-50 fixed justify-center items-center h-full w-full" style={{"zIndex": "1500", "display": "none"}}>
         <svg role="status" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-400 fill-indigo-600"
              viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
