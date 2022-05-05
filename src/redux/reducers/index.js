cdb50c13b20bef5131e052d3a4807c385294bcba
Ibtasham Ali <ibtasham.ali@emerssive.com>
2022-05-06 01:44:59 +0500

    adds institutes relevant reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,7 +2,8 @@ import {combineReducers} from "redux";
 import {setRolesReducer, setClassesReducer} from "./user-reducer";
 import {setSignupUser, setSigninUser} from "./auth-reducer";
 import {toasts} from './toast';
-import {setClassParticipantsReducer} from './participants-reducer'
+import {setClassParticipantsReducer} from './participants-reducer';
+import  {setInstitutesRequests, setInstitutes, setDepartmentsInInstitute} from './institute_reducer'
 
 const reducers = combineReducers({
   signup: setSignupUser,
@@ -10,6 +11,9 @@ const reducers = combineReducers({
   roles: setRolesReducer,
   classes: setClassesReducer,
   participants: setClassParticipantsReducer,
+  institute_requests: setInstitutesRequests,
+  institutes: setInstitutes,
+  departments: setDepartmentsInInstitute,
   toasts
 });
 
---
+++ src/redux/reducers/institute_reducer.js
@@ -0,0 +1,28 @@
+import {ActionTypes} from "../constants/actions-types";
+
+export const setInstitutes = (state = {}, action) => {
+  switch (action.type) {
+    case ActionTypes.GET_INSTITUTES_SUCCESS:
+      return { ...state, ...action.data };
+    default:
+      return state;
+  }
+};
+
+export const setInstitutesRequests = (state = {}, action) => {
+  switch (action.type) {
+    case ActionTypes.GET_INSTITUTE_REQUESTS_SUCCESS:
+      return { ...state, ...action.data };
+    default:
+      return state;
+  }
+};
+
+export const setDepartmentsInInstitute = (state = {}, action) => {
+  switch (action.type) {
+    case ActionTypes.GET_DEPARTMENTS_IN_INSTITUTE_SUCCESS:
+      return { ...state, ...action.data };
+    default:
+      return state;
+  }
+};
