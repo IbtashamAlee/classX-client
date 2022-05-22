8fa79f69d695617a3b9aab0c14eb1f2fde064075
Ibtasham Ali <ibtasham.ali@emerssive.com>
2022-05-23 04:32:26 +0500

    user profile

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -2,11 +2,7 @@ import {Fragment, useState} from 'react'
 import {Dialog, Transition} from '@headlessui/react'
 import {XIcon} from '@heroicons/react/outline'
 import {Header} from "../components/header";
-const user = {
-  name: 'Tom Cook',
-  imageUrl:
-    'https://media.istockphoto.com/photos/doctor-holding-digital-tablet-at-meeting-room-picture-id1189304032?k=20&m=1189304032&s=612x612&w=0&h=ovTNnR0JX2cRZkzMBed9exRO_PamZLlysLDFkXesr4Q=',
-}
+import {useSelector} from "react-redux";
 
 const profile = {
   name: 'Ricardo Cooper',
@@ -36,165 +32,170 @@ export default function Example() {
   const tabs = [
     {name: 'Profile', current: currentTab === "Profile"}
   ]
+
+  const user = useSelector((state => state.user.user));
+
   return (
     <div>
-    <Header/>
-  <div className="flex justify-center items-center h-full mx-3">
-      <div className="flex  w-full md:w-2/3 ld:w-1/2 shadow mt-10 rounded-b-md">
-        <Transition.Root show={sidebarOpen} as={Fragment}>
-          <Dialog as="div" className="fixed inset-0 flex z-40 lg:hidden" onClose={setSidebarOpen}>
-            <Transition.Child
-              as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75"/>
-            </Transition.Child>
-            <Transition.Child
-              as={Fragment}
-              enter="transition ease-in-out duration-300 transform"
-              enterFrom="-translate-x-full"
-              enterTo="translate-x-0"
-              leave="transition ease-in-out duration-300 transform"
-              leaveFrom="translate-x-0"
-              leaveTo="-translate-x-full"
-            >
-              <div
-                className="relative flex-1 flex flex-col max-w-xs w-full bg-white focus:outline-none">
+      <Header/>
+      <div className="flex justify-center items-center h-full mx-3">
+          <div className="flex  w-full md:w-2/3 ld:w-1/2 shadow mt-10 rounded-b-md">
+            <Transition.Root show={sidebarOpen} as={Fragment}>
+              <Dialog as="div" className="fixed inset-0 flex z-40 lg:hidden" onClose={setSidebarOpen}>
                 <Transition.Child
                   as={Fragment}
-                  enter="ease-in-out duration-300"
+                  enter="transition-opacity ease-linear duration-300"
                   enterFrom="opacity-0"
                   enterTo="opacity-100"
-                  leave="ease-in-out duration-300"
+                  leave="transition-opacity ease-linear duration-300"
                   leaveFrom="opacity-100"
                   leaveTo="opacity-0"
                 >
-                  <div className="absolute top-0 right-0 -mr-12 pt-2">
-                    <button
-                      type="button"
-                      className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                      onClick={() => setSidebarOpen(false)}
-                    >
-                      <span className="sr-only">Close sidebar</span>
-                      <XIcon className="h-6 w-6 text-white" aria-hidden="true"/>
-                    </button>
-                  </div>
+                  <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75"/>
                 </Transition.Child>
-                <div className="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
-                  <div className="shrink-0 flex items-center px-4">
-                    <img
-                      className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/workflow-logo-pink-500-mark-gray-900-text.svg"
-                      alt="Workflow"
-                    />
-                  </div>
-                </div>
-                <div className="shrink-0 flex border-t border-gray-200 p-4">
-                  <a href="#" className="shrink-0 group block">
-                    <div className="flex items-center">
-                      <div>
-                        <img className="inline-block h-10 w-10 rounded-full"
-                             src={user.imageUrl} alt=""/>
+                <Transition.Child
+                  as={Fragment}
+                  enter="transition ease-in-out duration-300 transform"
+                  enterFrom="-translate-x-full"
+                  enterTo="translate-x-0"
+                  leave="transition ease-in-out duration-300 transform"
+                  leaveFrom="translate-x-0"
+                  leaveTo="-translate-x-full"
+                >
+                  <div
+                    className="relative flex-1 flex flex-col max-w-xs w-full bg-white focus:outline-none">
+                    <Transition.Child
+                      as={Fragment}
+                      enter="ease-in-out duration-300"
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100"
+                      leave="ease-in-out duration-300"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <div className="absolute top-0 right-0 -mr-12 pt-2">
+                        <button
+                          type="button"
+                          className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                          onClick={() => setSidebarOpen(false)}
+                        >
+                          <span className="sr-only">Close sidebar</span>
+                          <XIcon className="h-6 w-6 text-white" aria-hidden="true"/>
+                        </button>
                       </div>
-                      <div className="ml-3">
-                        <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">{user.name}</p>
-                        <p className="text-sm font-medium text-gray-500 group-hover:text-gray-700">View
-                          profile</p>
-                      </div>
-                    </div>
-                  </a>
-                </div>
-              </div>
-            </Transition.Child>
-            <div className="shrink-0 w-14" aria-hidden="true">
-              {/* Force sidebar to shrink to fit close icon */}
-            </div>
-          </Dialog>
-        </Transition.Root>
-
-        <div className="flex flex-col min-w-0 flex-1 overflow-hidden">
-
-          <div className="flex-1 relative z-0 flex overflow-hidden">
-            <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none xl:order-last">
-
-              <article>
-                {/* Profile header */}
-                <div>
-                  <div>
-                    <img className="h-32 w-full object-cover lg:h-48 rounded-t-md"
-                         src={profile.coverImageUrl} alt=""/>
-                  </div>
-                  <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
-                      <div className="flex">
+                    </Transition.Child>
+                    <div className="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
+                      <div className="shrink-0 flex items-center px-4">
                         <img
-                          className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32 object-cover"
-                          src={profile.imageUrl}
-                          alt=""
+                          className="h-8 w-auto"
+                          src="https://tailwindui.com/img/logos/workflow-logo-pink-500-mark-gray-900-text.svg"
+                          alt="Workflow"
                         />
                       </div>
-                      <div
-                        className="mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
-                        <div className="sm:hidden 2xl:block mt-6 min-w-0 flex-1">
-                          <h1 className="text-2xl font-bold text-gray-900 truncate">{profile.name}</h1>
+                    </div>
+                    <div className="shrink-0 flex border-t border-gray-200 p-4">
+                      <a href="#" className="shrink-0 group block">
+                        <div className="flex items-center">
+                          <div>
+                            <img className="inline-block h-10 w-10 rounded-full"
+                                 src={user?.imageUrl ? user?.imageUrl: `${window.location.origin}/Sample_User_Icon.png`} alt=""/>
+                          </div>
+                          <div className="ml-3">
+                            <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">{user?.name}</p>
+                            <p className="text-sm font-medium text-gray-500 group-hover:text-gray-700">View
+                              profile</p>
+                          </div>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+                </Transition.Child>
+                <div className="shrink-0 w-14" aria-hidden="true">
+                  {/* Force sidebar to shrink to fit close icon */}
+                </div>
+              </Dialog>
+            </Transition.Root>
+
+            <div className="flex flex-col min-w-0 flex-1 overflow-hidden">
+
+              <div className="flex-1 relative z-0 flex overflow-hidden">
+                <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none xl:order-last">
+
+                  <article>
+                    {/* Profile header */}
+                    <div>
+                      <div>
+                        <img className="h-32 w-full object-cover lg:h-48 rounded-t-md"
+                             src={profile.coverImageUrl} alt=""/>
+                      </div>
+                      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+                        <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+                          <div className="flex">
+                            <img
+                              className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32 object-cover"
+                              src={user?.imageUrl ? user?.imageUrl: `${window.location.origin}/Sample_User_Icon.png`}
+                              alt=""
+                            />
+                          </div>
+                          <div
+                            className="mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+                            <div className="sm:hidden 2xl:block mt-6 min-w-0 flex-1">
+                              <h1 className="text-2xl font-bold text-gray-900 truncate">{user?.name}</h1>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="hidden sm:block 2xl:hidden mt-6 min-w-0 flex-1">
+                          <h1 className="text-2xl font-bold text-gray-900 truncate">{user?.name}</h1>
                         </div>
                       </div>
                     </div>
-                    <div className="hidden sm:block 2xl:hidden mt-6 min-w-0 flex-1">
-                      <h1 className="text-2xl font-bold text-gray-900 truncate">{profile.name}</h1>
-                    </div>
-                  </div>
-                </div>
 
-                {/* Tabs */}
-                <div className="mt-6 sm:mt-2 2xl:mt-5">
-                  <div className="border-b border-gray-200">
-                    <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
-                      <nav className="-mb-px flex space-x-8" aria-label="Tabs">
-                        {tabs.map((tab) => (
-                          <a
-                            key={tab.name}
-                            href={tab.href}
-                            onClick={() => {
-                              selectCurrentTab(tab.name)
-                            }}
-                            className={classNames(
-                              tab.current
-                                ? 'border-pink-500 text-gray-900'
-                                : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
-                              'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm'
-                            )}
-                            aria-current={tab.current ? 'page' : undefined}
-                          >
-                            {tab.name}
-                          </a>
-                        ))}
-                      </nav>
-                    </div>
-                  </div>
-                </div>
-                {currentTab === "Profile" && <div className="p-12 mt-6 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
-                  <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
-                    {Object.keys(profile.fields).map((field) => (
-                      <div key={field} className="sm:col-span-1">
-                        <dt className="text-sm font-medium text-gray-500">{field}</dt>
-                        <dd className="mt-1 text-sm text-gray-900">{(profile.fields)[field]}</dd>
+                    {/* Tabs */}
+                    <div className="mt-6 sm:mt-2 2xl:mt-5">
+                      <div className="border-b border-gray-200">
+                        <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+                          <nav className="-mb-px flex space-x-8" aria-label="Tabs">
+                            {tabs.map((tab) => (
+                              <a
+                                key={tab.name}
+                                href={tab.href}
+                                onClick={() => {
+                                  selectCurrentTab(tab.name)
+                                }}
+                                className={classNames(
+                                  tab.current
+                                    ? 'border-pink-500 text-gray-900'
+                                    : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
+                                  'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm'
+                                )}
+                                aria-current={tab.current ? 'page' : undefined}
+                              >
+                                {tab.name}
+                              </a>
+                            ))}
+                          </nav>
+                        </div>
                       </div>
-                    ))}
-                  </dl>
-                </div>}
+                    </div>
+                    {currentTab === "Profile" && <div className="p-12 mt-6 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+                      <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+                        <div className="sm:col-span-1">
+                          <dt className="text-sm font-medium text-gray-500">Email</dt>
+                          <dd className="mt-1 text-sm text-gray-900">{user?.email}</dd>
+                        </div>
+                        <div className="sm:col-span-1">
+                          <dt className="text-sm font-medium text-gray-500">Status</dt>
+                          <dd className="mt-1 text-sm text-gray-900">{user?.userStatus ?? "Hi, I'm using classX"}</dd>
+                        </div>
+                      </dl>
+                    </div>}
 
-              </article>
-            </main>
+                  </article>
+                </main>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    </div>
   )
 }
