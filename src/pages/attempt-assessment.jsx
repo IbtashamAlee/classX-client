d0ba64ec7f0b2faed181216da7d02c5341e17e9e
faseehahmad00 <faseehahmad00@gmail.com>
2022-06-13 13:16:58 +0500

    attempt assessment functionality

--- src/pages/attempt-assessment.jsx
+++ src/pages/attempt-assessment.jsx
@@ -11,6 +11,8 @@ import {FilePicker} from "../components/file-picker";
 import {Button} from "@mui/material";
 import {useEffect, useState} from "react";
 import api from '../generic-services/api'
+import done from '../done.svg';
+import {useNavigate} from 'react-router-dom';
 
 const assessment = {
   id: 3
@@ -78,21 +80,27 @@ function BpRadio(props) {
 }
 
 
-
 export default function AttemptAssessment() {
   const [questions,setQuestions] = useState([])
-  const [current,setCurrent] = useState(null)
+  const [current,setCurrent] = useState(0)
+  const [isMCQ, setIsMCQ] = useState(false)
+  let navigate = useNavigate();
 
   useEffect(()=>{
     api.execute("/class/assessment/"+assessment.id,'get')
       .then((res)=>{
         setQuestions(res.data.assessment.question)
-        localStorage.getItem("current") ? setCurrent(localStorage.getItem("current")) : localStorage.setItem("current",1)
+        localStorage.getItem("current"+assessment.id) ? setCurrent(localStorage.getItem("current"+assessment.id)) : localStorage.setItem("current"+assessment.id,0)
       })
   },[])
 
   useEffect(()=>{
     console.log(current)
+    let correct = 0
+    questions[current]?.option?.map(opt=>{
+      if(opt.isCorrect) correct++
+    })
+    if(correct === 1) setIsMCQ(true)
   },[current])
 
   const [content, setContent] = useState('');
@@ -102,80 +110,103 @@ export default function AttemptAssessment() {
     setFiles(files);
   }
 
+  if(current >= questions.length){
+    return (
+      <div className="">
+        <Header/>
+        <div className="min-h-[90vh] flex justify-center items-center flex-col">
+          <img src={done} className="h-1/2 w-1/2"/>
+          <p className="mt-4 font-semibold text-2xl">Assessment Attempted Successfully</p>
+          <Button onClick={()=>navigate('/')} variant='contained' style={{marginTop:'10px'}}>BACK TO DASHBOARD</Button>
+        </div>
+    </div>
+    )
+  }
   return (
     <React.Fragment>
       <Header/>
       <div className="bg-white flex flex-col items-center justify-center h-[90vh]">
         <div className="max-w-7xl py-8 px-4 sm:px-6 lg:py-16 lg:px-8 bg-slate-50 shadow w-full">
           <h2 className="text-sm tracking-tight text-gray-900 sm:text-xl font-bold">
-            <span className="block">QUESTION 1 :</span>
+            <span className="block">QUESTION {(current-1+2)} :</span>
           </h2>
-          <p className="text-2xl my-3">How many days in week ?</p>
+          <p className="text-2xl my-3">{questions[current]?.statement}</p>
 
+          {
+            questions && questions[current] && questions[current].option.length < 1 &&
+          <form action="#" className="relative">
+            <div className="border border-gray-300 rounded-lg shadow-sm overflow-hidden focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
+              <label htmlFor="comment" className="sr-only">
+                Add your comment
+              </label>
+              <textarea
+                rows={3}
+                name="comment"
+                id="comment"
+                className="block w-full py-3 border-0 resize-none focus:ring-0 sm:text-sm"
+                placeholder="Write you answer here...."
+                value={content}
+                onChange={(e) => {setContent(e.target.value)}}
+              />
+              <div className="py-2" aria-hidden="true">
+                <div className="py-px">
+                  <div className="h-9" />
+                </div>
+              </div>
+            </div>
 
-          {/*// IF MCQ*/}
-          {/*<FormControl onChange={(e)=>console.log(e.target.value)}>*/}
-          {/*  <RadioGroup*/}
-          {/*    aria-labelledby="demo-customized-radios"*/}
-          {/*    name="customized-radios"*/}
-          {/*  >*/}
-          {/*    <FormControlLabel value="7 days" control={<BpRadio />} label="7 days" />*/}
-          {/*    <FormControlLabel value="8 days" control={<BpRadio />} label="8 days" />*/}
-          {/*    <FormControlLabel value="9 days" control={<BpRadio />} label="9 days" />*/}
-          {/*  </RadioGroup>*/}
-          {/*</FormControl>*/}
+            <div className="absolute bottom-0 inset-x-0 pl-3 pr-2 py-2 flex justify-between">
+              <div className="flex items-center space-x-5">
+                <div className="flex items-center">
+                  <FilePicker fileReturn={getFiles}/>
+                </div>
+              </div>
+            </div>
+          </form>
+          }
 
+          {
+            questions && questions[current] && isMCQ &&
+            <FormControl onChange={(e)=>console.log(e.target.value)}>
+              <RadioGroup
+              aria-labelledby="demo-customized-radios"
+              name="customized-radios"
+            >
+              {
+                questions[current].option.map((opt)=>{
+                  return (<FormControlLabel value={opt.value} control={<BpRadio />} label={opt.value} />)
+                })
+              }
+            </RadioGroup>
+          </FormControl>
 
-          {/*//IF MULTIPLE ANSWERS TO MCQS*/}
-          {/*<div className="flex flex-col justify-start items-start">*/}
-          {/*<div className="flex inline-flex justify-center items-center">*/}
-          {/*  <Checkbox label="7 Days" value="123"  onChange={(e)=>console.log(e.target.value)} color="secondary" />*/}
-          {/*  <p>7 days</p>*/}
-          {/*</div>*/}
+          }
 
-          {/*<div className="flex inline-flex justify-center items-center">*/}
-          {/*  <Checkbox label="Seven" value="123"  onChange={(e)=>console.log(e.target.value)} color="secondary" />*/}
-          {/*  <p>Seven days</p>*/}
-          {/*</div>*/}
-          {/*</div>*/}
-
-          {/*//If Question is open ended */}
-          {/*<form action="#" className="relative">*/}
-          {/*  <div className="border border-gray-300 rounded-lg shadow-sm overflow-hidden focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">*/}
-          {/*    <label htmlFor="comment" className="sr-only">*/}
-          {/*      Add your comment*/}
-          {/*    </label>*/}
-          {/*    <textarea*/}
-          {/*      rows={3}*/}
-          {/*      name="comment"*/}
-          {/*      id="comment"*/}
-          {/*      className="block w-full py-3 border-0 resize-none focus:ring-0 sm:text-sm"*/}
-          {/*      placeholder="Write you answer here...."*/}
-          {/*      value={content}*/}
-          {/*      onChange={(e) => {setContent(e.target.value)}}*/}
-          {/*    />*/}
-          {/*    <div className="py-2" aria-hidden="true">*/}
-          {/*      <div className="py-px">*/}
-          {/*        <div className="h-9" />*/}
-          {/*      </div>*/}
-          {/*    </div>*/}
-          {/*  </div>*/}
-
-          {/*  <div className="absolute bottom-0 inset-x-0 pl-3 pr-2 py-2 flex justify-between">*/}
-          {/*    <div className="flex items-center space-x-5">*/}
-          {/*      <div className="flex items-center">*/}
-          {/*        <FilePicker fileReturn={getFiles}/>*/}
-          {/*      </div>*/}
-          {/*    </div>*/}
-          {/*  </div>*/}
-          {/*</form>*/}
-
+          {
+            questions && questions[current] && questions[current].option?.length > 0 && !isMCQ &&
+            <div className="flex flex-col justify-start items-start">
+              {
+                questions[current].option.map(opt => {
+                  return (
+                  <div className="flex inline-flex justify-center items-center">
+                    <Checkbox label={opt.value} value={opt.value} onChange={(e) => console.log(e.target.value)}
+                              color="secondary"/>
+                    <p>{opt.value}</p>
+                  </div>
+                  );
+                })
+              }
+            </div>
+          }
 
           <div className="mt-8 flex justify-center">
             <div className="inline-flex rounded-md shadow">
               <a
-                href="/attempt-assesment"
                 className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
+                onClick={()=> {
+                  localStorage.setItem("current" + assessment.id, current - 1 + 2)
+                  setCurrent(current-1+2)
+                }}
               >
                 Next
               </a>
