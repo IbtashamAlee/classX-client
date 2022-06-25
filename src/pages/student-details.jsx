3602a897bed3742f427b36860a712f34f35b7224
faseehahmad00 <faseehahmad00@gmail.com>
2022-06-25 17:49:31 +0500

    fixes

--- src/pages/student-details.jsx
+++ src/pages/student-details.jsx
@@ -339,7 +339,7 @@ export default function StudentDetails() {
                               {marks.map((marks,key) => (
                                 <tr key={key}>
                                   <td
-                                    className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 text-center">{marks.assessmentId}</td>
+                                    className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 text-center">{marks.assessment?.name}</td>
                                   <td
                                     className="whitespace-nowrap px-3 py-4 text-sm text-gray-500 text-center">{marks.classAssessmentSubmission[0]?.submittedAt?.split('T')[0]}</td>
                                   <td
