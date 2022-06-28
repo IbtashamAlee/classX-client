9b577c380ad289f2398824965f9d88d07e9c3fbc
Ibtasham Ali <ibtasham.ali@emerssive.com>
2022-06-29 02:53:38 +0500

    fixed
    Signed-off-by: Ibtasham Ali <ibtasham.ali@emerssive.com>

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -62,110 +62,110 @@ export default function Sidebar(props) {
     <>
       { Class &&
       <div>
-        <Transition.Root show={props.isOpen} as={Fragment}>
-          <Dialog as="div" className="fixed inset-0 z-40 flex md:hidden" onClose={props.setSidebarOpen}>
-            <Transition.Child
-              as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75"/>
-            </Transition.Child>
-            <Transition.Child
-              as={Fragment}
-              enter="transition ease-in-out duration-300 transform"
-              enterFrom="-translate-x-full"
-              enterTo="translate-x-0"
-              leave="transition ease-in-out duration-300 transform"
-              leaveFrom="translate-x-0"
-              leaveTo="-translate-x-full"
-            >
-              <div className="relative max-w-xs w-full bg-white pt-5 pb-4 flex-1 flex flex-col">
-                <Transition.Child
-                  as={Fragment}
-                  enter="ease-in-out duration-300"
-                  enterFrom="opacity-0"
-                  enterTo="opacity-100"
-                  leave="ease-in-out duration-300"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
-                >
-                  <div className="absolute top-0 right-0 -mr-12 pt-2">
-                    <button
-                      type="button"
-                      className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                      onClick={() => props.setSidebarOpen(false)}
-                    >
-                      <span className="sr-only">Close sidebar</span>
-                      <XIcon className="h-6 w-6 text-white" aria-hidden="true"/>
-                    </button>
-                  </div>
-                </Transition.Child>
-                <div className="flex-shrink-0 px-4 flex items-center">
-                  <img
-                    className="h-14 w-auto"
-                    src={window.location.origin + '/logo.svg'}
-                    alt="ClassX"
-                  />
-                </div>
-                <div className="mt-5 flex-1 h-0 overflow-y-auto">
-                  <nav className="px-2 space-y-1">
-                    <div className="text-[#6366F1] bg-blue-50 mb-2 mt-0 rounded-lg mx-1 px-2 py-1">
-                      <div className="flex flex-row justify-between">
-                        <h1 className="font-bold text-[#6366F1] ml-3">{Class.name}</h1>
-                        {!detailsExpanded &&
-                        <ExpandMoreIcon onClick={() => {
-                          setDetailsExpanded(!detailsExpanded)
-                        }}/>
-                        }
-                        {detailsExpanded &&
-                        <ExpandLessIcon onClick={() => setDetailsExpanded(!detailsExpanded)}/>
-                        }
-                      </div>
-                      {detailsExpanded &&
-                      <div className="flex flex-row justify-start items-center mt-1">
-                        <p className="text-xs ml-3">code: {Class.code}</p>
-                        <ContentCopyIcon style={{marginLeft: '10px', height: '14px'}} onClick={() => {
-                          navigator.clipboard.writeText(Class.code)
-                        }}/>
-                      </div>
-                      }
-                    </div>
-                    {navigation.map((item) => (
-                      <Link to={item.href} key={item.name} onClick={() => {
-                        changeNavigation(item.name)
-                      }}>
-                            <span
-                              className={classNames(
-                                item.current
-                                  ? 'bg-gray-100 text-gray-900'
-                                  : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
-                                'group rounded-md py-2 px-2 flex items-center text-base font-medium'
-                              )}
-                            >
-                            <item.icon
-                              className={classNames(
-                                item.current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500',
-                                'mr-4 flex-shrink-0 h-6 w-6'
-                              )}
-                              aria-hidden="true"
-                            />
-                              {item.name}
-                          </span>
-                      </Link>
-                    ))}
-                  </nav>
-                </div>
-              </div>
-            </Transition.Child>
-            <div
-              className="flex-shrink-0 w-14">{/* Dummy element to force sidebar to shrink to fit close icon */}</div>
-          </Dialog>
-        </Transition.Root>
+        {/*<Transition.Root show={props.isOpen} as={Fragment}>*/}
+        {/*  <Dialog as="div" className="fixed inset-0 z-40 flex md:hidden" onClose={props.setSidebarOpen}>*/}
+        {/*    <Transition.Child*/}
+        {/*      as={Fragment}*/}
+        {/*      enter="transition-opacity ease-linear duration-300"*/}
+        {/*      enterFrom="opacity-0"*/}
+        {/*      enterTo="opacity-100"*/}
+        {/*      leave="transition-opacity ease-linear duration-300"*/}
+        {/*      leaveFrom="opacity-100"*/}
+        {/*      leaveTo="opacity-0"*/}
+        {/*    >*/}
+        {/*      <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75"/>*/}
+        {/*    </Transition.Child>*/}
+        {/*    <Transition.Child*/}
+        {/*      as={Fragment}*/}
+        {/*      enter="transition ease-in-out duration-300 transform"*/}
+        {/*      enterFrom="-translate-x-full"*/}
+        {/*      enterTo="translate-x-0"*/}
+        {/*      leave="transition ease-in-out duration-300 transform"*/}
+        {/*      leaveFrom="translate-x-0"*/}
+        {/*      leaveTo="-translate-x-full"*/}
+        {/*    >*/}
+        {/*      <div className="relative max-w-xs w-full bg-white pt-5 pb-4 flex-1 flex flex-col">*/}
+        {/*        <Transition.Child*/}
+        {/*          as={Fragment}*/}
+        {/*          enter="ease-in-out duration-300"*/}
+        {/*          enterFrom="opacity-0"*/}
+        {/*          enterTo="opacity-100"*/}
+        {/*          leave="ease-in-out duration-300"*/}
+        {/*          leaveFrom="opacity-100"*/}
+        {/*          leaveTo="opacity-0"*/}
+        {/*        >*/}
+        {/*          <div className="absolute top-0 right-0 -mr-12 pt-2">*/}
+        {/*            <button*/}
+        {/*              type="button"*/}
+        {/*              className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"*/}
+        {/*              onClick={() => props.setSidebarOpen(false)}*/}
+        {/*            >*/}
+        {/*              <span className="sr-only">Close sidebar</span>*/}
+        {/*              <XIcon className="h-6 w-6 text-white" aria-hidden="true"/>*/}
+        {/*            </button>*/}
+        {/*          </div>*/}
+        {/*        </Transition.Child>*/}
+        {/*        <div className="flex-shrink-0 px-4 flex items-center">*/}
+        {/*          <img*/}
+        {/*            className="h-14 w-auto"*/}
+        {/*            src={window.location.origin + '/logo.svg'}*/}
+        {/*            alt="ClassX"*/}
+        {/*          />*/}
+        {/*        </div>*/}
+        {/*        <div className="mt-5 flex-1 h-0 overflow-y-auto">*/}
+        {/*          <nav className="px-2 space-y-1">*/}
+        {/*            <div className="text-[#6366F1] bg-blue-50 mb-2 mt-0 rounded-lg mx-1 px-2 py-1">*/}
+        {/*              <div className="flex flex-row justify-between">*/}
+        {/*                <h1 className="font-bold text-[#6366F1] ml-3">{Class.name}</h1>*/}
+        {/*                {!detailsExpanded &&*/}
+        {/*                <ExpandMoreIcon onClick={() => {*/}
+        {/*                  setDetailsExpanded(!detailsExpanded)*/}
+        {/*                }}/>*/}
+        {/*                }*/}
+        {/*                {detailsExpanded &&*/}
+        {/*                <ExpandLessIcon onClick={() => setDetailsExpanded(!detailsExpanded)}/>*/}
+        {/*                }*/}
+        {/*              </div>*/}
+        {/*              {detailsExpanded &&*/}
+        {/*              <div className="flex flex-row justify-start items-center mt-1">*/}
+        {/*                <p className="text-xs ml-3">code: {Class.code}</p>*/}
+        {/*                <ContentCopyIcon style={{marginLeft: '10px', height: '14px'}} onClick={() => {*/}
+        {/*                  navigator.clipboard.writeText(Class.code)*/}
+        {/*                }}/>*/}
+        {/*              </div>*/}
+        {/*              }*/}
+        {/*            </div>*/}
+        {/*            {navigation.map((item) => (*/}
+        {/*              <Link to={item.href} key={item.name} onClick={() => {*/}
+        {/*                changeNavigation(item.name)*/}
+        {/*              }}>*/}
+        {/*                    <span*/}
+        {/*                      className={classNames(*/}
+        {/*                        item.current*/}
+        {/*                          ? 'bg-gray-100 text-gray-900'*/}
+        {/*                          : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',*/}
+        {/*                        'group rounded-md py-2 px-2 flex items-center text-base font-medium'*/}
+        {/*                      )}*/}
+        {/*                    >*/}
+        {/*                    <item.icon*/}
+        {/*                      className={classNames(*/}
+        {/*                        item.current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500',*/}
+        {/*                        'mr-4 flex-shrink-0 h-6 w-6'*/}
+        {/*                      )}*/}
+        {/*                      aria-hidden="true"*/}
+        {/*                    />*/}
+        {/*                      {item.name}*/}
+        {/*                  </span>*/}
+        {/*              </Link>*/}
+        {/*            ))}*/}
+        {/*          </nav>*/}
+        {/*        </div>*/}
+        {/*      </div>*/}
+        {/*    </Transition.Child>*/}
+        {/*    <div*/}
+        {/*      className="flex-shrink-0 w-14">/!* Dummy element to force sidebar to shrink to fit close icon *!/</div>*/}
+        {/*  </Dialog>*/}
+        {/*</Transition.Root>*/}
 
         {/* Static sidebar for desktop */}
         <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
