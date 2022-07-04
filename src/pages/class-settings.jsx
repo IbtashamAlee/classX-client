04ac51faabf54685de9d721323ac2d449ccfca5f
faseehahmad00 <faseehahmad00@gmail.com>
2022-07-04 17:39:08 +0500

    added class code and update option in class settings

--- src/pages/class-settings.jsx
+++ src/pages/class-settings.jsx
@@ -8,7 +8,7 @@ import {useParams} from "react-router-dom";
 import {useSelector} from "react-redux";
 import api from "../generic-services/api";
 import {useNavigate} from 'react-router-dom'
-
+import UpdateIcon from '@mui/icons-material/Update';
 
 export default function ClassSettings() {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,7 +17,8 @@ export default function ClassSettings() {
 
   const [name, setName] =  useState('');
   const [description, setDescription] =  useState('');
-
+  const [code,setCode] = useState('')
+  const [key,setKey] = useState(0)
   let {id} = useParams();
 
   let handleClose = () => {
@@ -27,6 +28,7 @@ export default function ClassSettings() {
   useEffect(() => {
     setName(current_class?.name)
     setDescription(current_class?.description)
+    setCode(current_class?.code)
   }, [current_class])
 
   useEffect(()=>{
@@ -46,12 +48,18 @@ export default function ClassSettings() {
     })
   }
 
+  function updateCode(){
+    api.execute(`/api/class/${id}/class-code`,'PUT')
+      .then(res=>setCode(res.data.code))
+      .catch(err => console.log(err))
+  }
+
   let updateClass = () => {
 
   }
 
   return (
-    <div>
+    <div key={key}>
       <main className="mt-10">
         <div className="max-w-screen-xl mx-auto pb-6 px-4 sm:px-6 lg:pb-16 lg:px-8">
           <div className="bg-white rounded-lg shadow overflow-hidden">
@@ -90,6 +98,20 @@ export default function ClassSettings() {
                             value={description}
                             onChange={e => {setDescription(e.target.value)}}
                         />
+                        <div className="flex flex-row justify-start items-center">
+                        <TextValidator
+                          id="code"
+                          name="code"
+                          minRows={1}
+                          maxRows={1}
+                          label={"Class Code"}
+                          className="!w-full !col-span-3"
+                          value={code}
+                          disabled={true}
+                        />
+                        <UpdateIcon className="ml-4 hover:fill-blue-700 hover:scale-[1.1]" onClick={updateCode}
+                        />
+                        </div>
                       </div>
                     </div>
 
@@ -114,12 +136,6 @@ export default function ClassSettings() {
                                 <span>Change</span>
                                 <span className="sr-only"> user photo</span>
                               </label>
-                              {/*<input*/}
-                              {/*  id="mobile-user-photo"*/}
-                              {/*  name="user-photo"*/}
-                              {/*  type="file"*/}
-                              {/*  className="absolute w-full h-full opacity-0 cursor-pointer border-gray-300 rounded-md"*/}
-                              {/*/>*/}
                             </div>
                           </div>
                         </div>
