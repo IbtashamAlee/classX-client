849ba935333684d9707b7d963ed303cadce23b7a
Ibtasham Ali <ibtasham.ali@emerssive.com>
2022-07-06 18:48:28 +0500

    update institue settings
    Signed-off-by: Ibtasham Ali <ibtasham.ali@emerssive.com>

--- src/pages/institute-settings.jsx
+++ src/pages/institute-settings.jsx
@@ -5,6 +5,8 @@ import {Header} from '../components/header'
 import api from "../generic-services/api";
 import {useParams} from "react-router-dom";
 import Chart from "react-apexcharts";
+import {Button} from "@mui/material";
+import {FilePicker} from "../components/file-picker";
 
 
 
@@ -50,7 +52,15 @@ export default function InstituteSettings() {
   })
   const [series, setSeries] = useState(null)
   const [data,setData] = useState(null)
-  const {id} = useParams()
+  const {id} = useParams();
+
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [country, setCountry] = useState('');
+  const [street, setStreet] = useState("");
+  const [city, setCity] = useState("");
+  const [imageUrl, setImageUrl] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(()=>{
     api.execute(`/api/stats/institute/${id}/attendance-stats`)
@@ -58,20 +68,70 @@ export default function InstituteSettings() {
       .catch(e => console.log(e))
   },[])
 
-  useEffect(()=>{
+  const getInstitute = () => {
     api.execute('/api/institutes/'+id)
-      .then(res=> setData(res.data))
-      .catch(e=>console.log(e))
+        .then(res=> {
+          setData(res.data)
+          let ins = res.data;
+          setName(ins.name);
+          setDescription(ins.description);
+          setStreet(ins.address);
+          setCity(ins.city);
+          setCountry(ins.country);
+          setImageUrl(ins.imageUrl);
+        }).catch( e => {
+          console.log(e)
+        }
+    )
+  }
+
+  const updateInstitute = (e) => {
+    e.preventDefault();
+    api.execute('/api/institutes/'+id, 'put', {
+      "name": name,
+      "address": street,
+      "city": city,
+      "country": country,
+      "description": description,
+    }).then(res=> {
+      getInstitute();
+    }).catch( e => {
+        console.log(e)
+      }
+    )
+  }
+
+  let handleClose = () => {
+    setIsOpen(!isOpen);
+  }
+
+  const updateImage = (files) => {
+    console.log(files)
+    api.execute('/api/institutes/'+id, 'put', {
+      "imageUrl": files[0].publicUrl
+    }).then(res=> {
+      getInstitute();
+    }).catch( e => {
+          console.log(e)
+        }
+    )
+  }
+
+  useEffect(()=>{
+    getInstitute();
   },[])
   return (
     <div>
       <Header/>
-      {data &&
+      {isOpen &&
+          <FilePicker accept={true} open={isOpen} close={handleClose} fileReturn={updateImage}/>
+      }
+      {
         <main className="mt-10">
           <div className="max-w-screen-xl mx-auto pb-6 px-4 sm:px-6 lg:pb-16 lg:px-8">
             <div className="bg-white rounded-lg shadow overflow-hidden">
               <div className="divide-y divide-gray-200 lg:grid lg:grid-cols-9 lg:divide-y-0 lg:divide-x">
-                <form className="divide-y divide-gray-200 lg:col-span-9" action="#" method="POST">
+                <form className="divide-y divide-gray-200 lg:col-span-9" onSubmit={updateInstitute}>
                   {/* Profile section */}
                   <div className="py-6 px-4 sm:p-6 lg:pb-8">
                     <div>
@@ -94,7 +154,8 @@ export default function InstituteSettings() {
                               id="classname"
                               autoComplete="classname"
                               className="focus:ring-sky-500 focus:border-sky-500 flex-grow block w-full min-w-0 rounded-none rounded-r-md sm:text-sm border-gray-300 bg-gray-200"
-                              defaultValue={data.name}
+                              value={name}
+                              onChange={e => {setName(e.target.value)}}
                               disabled={true}
                             />
                           </div>
@@ -111,7 +172,8 @@ export default function InstituteSettings() {
                               id="classname"
                               autoComplete="classname"
                               className="focus:ring-sky-500 focus:border-sky-500 flex-grow block w-full min-w-0 rounded-none rounded-r-md sm:text-sm border-gray-300"
-                              defaultValue={data.address}
+                              value={street}
+                              onChange={e => {setStreet(e.target.value)}}
                             />
                           </div>
                         </div>
@@ -127,7 +189,8 @@ export default function InstituteSettings() {
                               id="classname"
                               autoComplete="classname"
                               className="focus:ring-sky-500 focus:border-sky-500 flex-grow block w-full min-w-0 rounded-none rounded-r-md sm:text-sm border-gray-300"
-                              defaultValue={data.country}
+                              value={country}
+                              onChange={e => {setCountry(e.target.value)}}
                             />
                           </div>
                         </div>
@@ -143,7 +206,8 @@ export default function InstituteSettings() {
                               id="classname"
                               autoComplete="classname"
                               className="focus:ring-sky-500 focus:border-sky-500 flex-grow block w-full min-w-0 rounded-none rounded-r-md sm:text-sm border-gray-300"
-                              defaultValue={data.city}
+                              value={city}
+                              onChange={e => {setCity(e.target.value)}}
                             />
                           </div>
                         </div>
@@ -159,7 +223,8 @@ export default function InstituteSettings() {
                             name="about"
                             rows={3}
                             className="shadow-sm focus:ring-sky-500 focus:border-sky-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
-                            defaultValue={data.description}
+                            value={description}
+                            onChange={e => {setDescription(e.target.value)}}
                           />
                           </div>
                           <p className="mt-2 text-sm text-gray-500">
@@ -172,13 +237,13 @@ export default function InstituteSettings() {
                         <p className="text-sm font-medium text-gray-700" aria-hidden="true">
                           Photo
                         </p>
-                        <div className="mt-1 lg:hidden">
+                        <div className="mt-1 lg:hidden" onClick={() => {setIsOpen(!isOpen)}}>
                           <div className="flex items-center">
                             <div
                               className="flex-shrink-0 inline-block rounded-full overflow-hidden h-12 w-12"
                               aria-hidden="true"
                             >
-                              <img className="rounded-full h-full w-full" src={data.imageUrl} alt=""/>
+                              <img className="rounded-full h-full w-full" src={imageUrl} alt=""/>
                             </div>
                             <div className="ml-5 rounded-md shadow-sm">
                               <div
@@ -190,31 +255,19 @@ export default function InstituteSettings() {
                                   <span>Change</span>
                                   <span className="sr-only"> user photo</span>
                                 </label>
-                                <input
-                                  id="mobile-user-photo"
-                                  name="user-photo"
-                                  type="file"
-                                  className="absolute w-full h-full opacity-0 cursor-pointer border-gray-300 rounded-md"
-                                />
                               </div>
                             </div>
                           </div>
                         </div>
 
-                        <div className="hidden relative rounded-full overflow-hidden lg:block">
-                          <img className="relative rounded-full w-40 h-40" src={data.imageUrl} alt=""/>
+                        <div className="hidden relative rounded-full overflow-hidden lg:block" onClick={() => {setIsOpen(!isOpen)}}>
+                          <img className="relative rounded-full w-40 h-40" src={imageUrl} alt=""/>
                           <label
                             htmlFor="desktop-user-photo"
                             className="absolute inset-0 w-full h-full bg-black bg-opacity-75 flex items-center justify-center text-sm font-medium text-white opacity-0 hover:opacity-100 focus-within:opacity-100"
                           >
                             <span>Change</span>
                             <span className="sr-only"> user photo</span>
-                            <input
-                              type="file"
-                              id="desktop-user-photo"
-                              name="user-photo"
-                              className="absolute inset-0 w-full h-full opacity-0 cursor-pointer border-gray-300 rounded-md"
-                            />
                           </label>
                         </div>
                       </div>
@@ -224,18 +277,12 @@ export default function InstituteSettings() {
                   {/* Privacy section */}
                   <div className="pt-6 divide-y divide-gray-200">
                     <div className="mt-0 py-4 px-4 flex justify-end sm:px-6">
-                      <button
-                        type="button"
-                        className="bg-white border border-gray-300 rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
-                      >
-                        Cancel
-                      </button>
-                      <button
+                      <Button
                         type="submit"
-                        className="ml-5 bg-sky-700 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-sky-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500"
+                        variant={"contained"}
                       >
                         Save
-                      </button>
+                      </Button>
                     </div>
                   </div>
                 </form>
